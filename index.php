d8c3ccfc08440a28ac15a5218be558b2077c7300
intercision <matt.saul@gmail.com>
2016-12-03 14:44:15 -0500

    This is a test.

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+  require('config.inc');
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Sign These Guys</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=yes" />
+
+<style type="text/css">
+
+body {
+  font-family: Arial, Helvetica, sans-serif;	
+}
+
+#wrapper {
+ max-width: 700px;	
+	
+}
+
+</style>
+
+</head>
+
+<body>
+
+<div id="wrapper">
+<h1>Sign These Guys!</h1>
+<p>Not everybody who makes music gets played on the radio but thanks to the internet you can hear them anyway!</p>
+<p>&nbsp;</p>
+<h3>Get in the Christmas spirit with <a href="https://www.youtube.com/channel/UCn4uD3E6QRZiCfBWAk30YeA">The Golden Flamers</a></h3>
+<p>&nbsp;</p>
+<p>We start out with salacious adventures under the mistletoe</p>
+<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/ISkiwyniqz0' frameborder='0' allowfullscreen></iframe></div>
+<p>&nbsp;</p>
+<p>Watch out for Jeff the Christmas Grinch, he's hangs elves instead of stockings.<br />
+</p>
+<p><style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed//j2FFqitK1Yg' frameborder='0' allowfullscreen></iframe></div>
+</p>
+
+
+<p><?php echo WHERE; ?>
+</p>
+<h3>Will DeVoursney - I'll Be Your Vampire</h3>
+<p>If you'd like a lover who would nibble on your neck, I'll be your vampire.</p>
+<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/87632596&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</div>
+
+</body>
+</html>
