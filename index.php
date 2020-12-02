8341bc4223f2646b889a281e4a80240afdc72749
intercision <matt.saul@gmail.com>
2020-12-02 15:31:28 -0500

    Squibble

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Sign These Guys</title>
+<title>Sign These Guys!</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=yes" />
 
 <style type="text/css">
